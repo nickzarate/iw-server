95c4d08a42ab8aef09d6a7a38b55d0dd4e99da63
Nick <nicholas.zarate@gmail.com>
2017-07-21 14:59:01 -0600

    insignificant chnage

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ var dbPassword = 'turnapp-tester'
 module.exports = {
   name: 'iw-server',
   version: '0.0.1',
-  env: 'development', // || process.env.NODE_ENV
-  port: 3000, // || process.env.PORT
+  env:/* process.env.NODE_ENV || */'development',
+  port:/* process.env.PORT || */3000,
   db: {
     uri: 'mongodb://' + dbUsername + ':' + dbPassword + '@iron-will-cluster-shard-00-00-tvtym.mongodb.net:27017,iron-will-cluster-shard-00-01-tvtym.mongodb.net:27017,iron-will-cluster-shard-00-02-tvtym.mongodb.net:27017/' + dbName + '?ssl=true&replicaSet=iron-will-cluster-shard-0&authSource=admin',
   }
